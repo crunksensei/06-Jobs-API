7f5501fd406846f6e3c51e945af3cb89d2c8228d
crunksensei <63222538+crunksensei@users.noreply.github.com>
2024-05-12 17:11:44 -0500

    dwasd

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 require('express-async-errors');
+
 const express = require('express');
 const app = express();
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 const connectDB = require('./db/connect');
 const authenticateUser = require('./middleware/authentication')
+
+
+//swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -33,9 +41,11 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
-
+app.get('/' , (req, res) => {
+  res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>')
+})
 // routes
-
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
